38a415a5ebe6c8eafdb52d0ed1eda5ada3345593
Michael Pan <panmpan@gmail.com>
2020-05-24 16:37:43 +0800

    Add test scene, controller

---
+++ Assets/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager ins;
+
+    [SerializeField]
+    private TileBase markerTile;
+
+    [SerializeField]
+    private Tilemap markerMap;
+
+    private void Awake() {
+        ins = this;
+    }
+
+    public void MarkTiles(Vector3Int[] tiles) {
+        for (var i = 0; i < tiles.Length; i++) {
+            markerMap.SetTile(tiles[i], markerTile);
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NavigationWalker))]
+public class PlayerController : MonoBehaviour
+{
+    private NavigationWalker walker;
+
+    [SerializeField]
+    private GameObject mousePositionIndicator;
+
+    private void Awake() {
+        walker = GetComponent<NavigationWalker>();
+    }
+
+    private void Update() {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int pointing;
+
+        if (NavigationManager.ins.FindBlockUnderPosition(mousePos, out pointing)) {
+            if (!mousePositionIndicator.activeSelf)
+                mousePositionIndicator.SetActive(true);
+
+            mousePositionIndicator.transform.position =  NavigationManager.ins.EnvMap.CellToWorld(pointing) + new Vector3(0.5f, 0.5f);
+
+            if (Input.GetMouseButtonDown(0)) {
+                walker.TryWalkTo(pointing);
+            }
+        }
+        else {
+            if (mousePositionIndicator.activeSelf)
+                mousePositionIndicator.SetActive(false);
+        }
+    }
+}
