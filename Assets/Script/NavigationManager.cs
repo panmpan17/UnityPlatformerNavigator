b530dd13079f1d99f263c89f28660a2676d31165
Michael Pan <panmpan@gmail.com>
2020-05-24 16:36:12 +0800

    Add Navigation walker, manager

---
+++ Assets/Script/NavigationManager.cs
@@ -0,0 +1,315 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class NavigationManager : MonoBehaviour
+{
+    static private List<T> CloneList<T>(List<T> old)
+    {
+        List<T> newList = new List<T>();
+        for (int i = 0; i < old.Count; i++)
+            newList.Add(old[i]);
+        return newList;
+    }
+
+    public static NavigationManager ins;
+
+    private static Vector3Int[] Around = new Vector3Int[] {
+        new Vector3Int(-1, -1, 0),
+        new Vector3Int(0, -1, 0),
+        new Vector3Int(1, -1, 0),
+
+        new Vector3Int(-1, 0, 0),
+        new Vector3Int(1, 0, 0),
+
+        new Vector3Int(-1, 1, 0),
+        new Vector3Int(0, 1, 0),
+        new Vector3Int(1, 1, 0),
+    };
+
+    public Tilemap EnvMap;
+    public Tilemap LadderMap;
+
+    private Dictionary<Vector3Int, Ground> grounds;
+
+    private void Awake() {
+        ins = this;
+
+        grounds = new Dictionary<Vector3Int, Ground>();
+        ScanGroundConnectivity();
+    }
+
+    private void ScanGroundConnectivity() {
+        BoundsInt bounds = EnvMap.cellBounds;
+
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            bool topWasAir = true;
+            int height = 0;
+            for (int y = bounds.yMax; y > bounds.yMin; y--)
+            {
+                Vector3Int pos = new Vector3Int(x, y, 0);
+                if (topWasAir)
+                {
+                    if (EnvMap.HasTile(pos))
+                    {
+                        grounds.Add(pos, new Ground(pos, height, EnvMap.CellToWorld(pos)));
+                        height = 0;
+                        topWasAir = false;
+                    }
+                    else
+                        height++;
+                }
+                else
+                {
+                    if (!EnvMap.HasTile(pos))
+                    {
+                        topWasAir = true;
+                        height = 1;
+                    }
+                }
+            }
+        }
+
+        foreach (KeyValuePair<Vector3Int, Ground> pair in grounds)
+        {
+            List<Ground> connections = new List<Ground>();
+            for (int i = 0; i < Around.Length; i++)
+            {
+                if (grounds.ContainsKey(pair.Key + Around[i]))
+                {
+                    connections.Add(grounds[pair.Key + Around[i]]);
+                }
+            }
+
+            pair.Value.connections = connections.ToArray();
+        }
+    }
+
+    public bool FindBlockUnderPosition(Vector3 pos, out Vector3Int position) {
+        position = EnvMap.WorldToCell(pos);
+
+        if (LadderMap.HasTile(position))
+            return true;
+
+        BoundsInt bounds = EnvMap.cellBounds;
+
+        for (int y = position.y; y > bounds.yMin; y--)
+        {
+            position.y = y;
+            if (grounds.ContainsKey(position))
+                return true;
+        }
+
+        return false;
+    }
+
+    public Route[] FindNavigateRoute(Vector3Int start, Vector3Int end) {
+        List<Route> routes;
+        if (!grounds.ContainsKey(start)) {
+            if (LadderMap.HasTile(start))
+                routes = new List<Route>() { new Route(start, RouteType.Ladder) };
+            else
+                return new Route[0];
+        }
+        else
+            routes = new List<Route>() { new Route(start, RouteType.Ground) };
+
+        if (grounds.ContainsKey(end) || LadderMap.HasTile(end)) {
+            List<Vector3Int> routeRepeatChecking = new List<Vector3Int>();
+            routeRepeatChecking.Add(start);
+            List<Route> connections = FindConnections(routeRepeatChecking, start, end);
+
+            for (int i = 0; i < connections.Count; i++)
+                routes.Add(connections[i]);
+        }
+
+        return routes.ToArray();
+    }
+
+    public List<Route> FindConnections(List<Vector3Int> routes, Vector3Int startPos, Vector3Int endPos, int count=0) {
+        if (count > 300) {
+            Debug.Log("REcursive overflow");
+            return new List<Route>();
+        }
+
+        if (grounds.ContainsKey(startPos)) {
+            Ground start = grounds[startPos];
+            for (int i = 0; i < start.connections.Length; i++)
+            {
+                if (routes.Contains(start.connections[i].Position))
+                    continue;
+
+                if (start.connections[i].Position != endPos)
+                {
+                    List<Vector3Int> routesCopy = CloneList(routes);
+                    routesCopy.Add(start.connections[i].Position);
+
+                    List<Route> _newRoute = FindConnections(routesCopy, start.connections[i].Position, endPos, count + 1);
+
+                    if (_newRoute.Count > 0) {
+                        List<Route> newRoutes = new List<Route>();
+                        newRoutes.Add(new Route(start.connections[i].Position, RouteType.Ground));
+                        newRoutes.AddRange(_newRoute);
+                        return newRoutes;
+                    }
+                }
+                else {
+                    return new List<Route>() { new Route(start.connections[i].Position, RouteType.Ground) };
+                }
+            }
+        }
+
+        // Find ladder go down
+        BoundsInt ladderBound = LadderMap.cellBounds;
+        if (LadderMap.HasTile(startPos))
+        {
+            List<Vector3Int> routesCopy = CloneList(routes);
+            List<Route> ladders = new List<Route>();
+
+            for (int y = startPos.y; y >= ladderBound.min.y - 1; y--) {
+                Vector3Int pos = new Vector3Int(startPos.x, y, 0);
+                routesCopy.Add(pos);
+                ladders.Add(new Route(pos, RouteType.Ladder));
+
+                if (endPos == pos)
+                {
+                    return ladders;
+                }
+
+                if (grounds.ContainsKey(pos))
+                {
+                    // if ground just hit
+                    routesCopy.Add(pos);
+                    List<Route> _newRoute = FindConnections(routesCopy, pos, endPos, count + 1);
+
+                    if (_newRoute.Count > 0)
+                    {
+                        List<Route> newRoute = new List<Route>();
+                        newRoute.AddRange(ladders);
+                        newRoute.Add(new Route(pos, RouteType.Ground));
+                        newRoute.AddRange(_newRoute);
+                        return newRoute;
+                    }
+                }
+                else if (!LadderMap.HasTile(pos))
+                    break;
+            }
+        }
+
+        // Find ladder go up
+        startPos.y += 1;
+        if (!routes.Contains(startPos) && LadderMap.HasTile(startPos))
+        {
+            List<Vector3Int> routesCopy = CloneList(routes);
+            List<Route> ladders = new List<Route>();
+
+            for (int y = startPos.y; y <= ladderBound.max.y; y++)
+            {
+                Vector3Int pos = new Vector3Int(startPos.x, y, 0);
+
+                if (LadderMap.HasTile(pos))
+                {
+                    routesCopy.Add(pos);
+                    ladders.Add(new Route(pos, RouteType.Ladder));
+
+                    if (endPos == pos) {
+                        if (grounds.ContainsKey(pos)) {
+                            ladders.Add(new Route(pos + new Vector3Int(0, 1, 0), RouteType.Ladder));
+                            ladders.Add(new Route(pos, RouteType.Ground));
+                        }
+                        return ladders;
+                    }
+
+                    if (grounds.ContainsKey(pos))
+                    {
+                        List<Route> _newRoutes = FindConnections(routesCopy, pos, endPos, count + 1);
+
+                        if (_newRoutes.Count > 0)
+                        {
+                            List<Route> newRoute = new List<Route>();
+                            newRoute.AddRange(ladders);
+                            newRoute.Add(new Route(pos + new Vector3Int(0, 1, 0), RouteType.Ladder));
+                            newRoute.Add(new Route(pos, RouteType.Ground));
+                            newRoute.AddRange(_newRoutes);
+                            return newRoute;
+                        }
+                    }
+                }
+                else
+                    break;
+            }
+        }
+
+        return new List<Route>();
+    }
+
+    public class Ground
+    {
+        public Vector3Int Position;
+        public int Height;
+
+        public Ground[] connections;
+
+        public Vector3 actualPosition;
+
+        public Ground(Vector3Int pos, int height, Vector3 _pos)
+        {
+            Position = pos;
+            Height = height;
+
+            actualPosition = _pos;
+        }
+    }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (grounds != null)
+        {
+            Vector3 offset = new Vector3(0.5f, 0.95f);
+
+            foreach (KeyValuePair<Vector3Int, Ground> pair in grounds)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawCube(pair.Value.actualPosition + offset, new Vector3(1, 0.1f));
+
+                Gizmos.color = Color.green;
+                for (int i = 0; i < pair.Value.connections.Length; i++)
+                {
+                    Gizmos.DrawLine(pair.Value.actualPosition + offset,
+                                    pair.Value.connections[i].actualPosition + offset);
+                }
+            }
+        }
+    }
+    #endif
+
+}
+
+public enum RouteType
+{
+    Ground,
+    Ladder,
+}
+public struct Route : System.IEquatable<Route>, System.IEquatable<Vector3Int>
+{
+    public Vector3Int Position;
+    public RouteType Type;
+
+    public Route(Vector3Int position, RouteType type)
+    {
+        Position = position;
+        Type = type;
+    }
+
+    public bool Equals(Route other)
+    {
+        return Position == other.Position && Type == other.Type;
+    }
+    public bool Equals(Vector3Int other)
+    {
+        return Position == other;
+    }
+}
---
+++ Assets/Script/NavigationWalker.cs
@@ -0,0 +1,281 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MPJamPack;
+using MPJamPack.Aseprite;
+
+public class NavigationWalker : MonoBehaviour
+{
+    private const string AKIdle = "Idle",
+                         AKWalk = "Walk",
+                         AKJump = "Jump",
+                         AKLand = "Land",
+                         AKClimb = "Climb",
+                         AKClimbIdle = "ClimbIdle";
+
+    private AseAnimator animator;
+    private SmartBoxCollider boxCollider;
+    private new Rigidbody2D rigidbody;
+    private float gravityScale;
+
+    [SerializeField]
+    private bool spriteFaceRight;
+    [SerializeField]
+    private float walkSpeed, climbSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float airSpeedMultiplier;
+    [SerializeField]
+    private Timer landWait;
+    private bool moving, jumping, jumpStart, climbing;
+
+    private bool waitOnGround;
+    private Vector3Int destination;
+
+    [SerializeField]
+    private GameObject destinationIndicator;
+
+    private Route[] routes;
+    private Vector3[] routePositions;
+    private int routeI;
+
+    private void Awake() {
+        animator = GetComponent<AseAnimator>();
+        boxCollider = GetComponent<SmartBoxCollider>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        gravityScale = rigidbody.gravityScale;
+
+        landWait.Running = false;
+    }
+
+    private void Turn(int direction) {
+        if (direction == 0)
+            return;
+
+        if (spriteFaceRight)
+        {
+            bool nowFaceRight = transform.localScale.x > 0;
+            if (nowFaceRight != direction > 0)
+                transform.localScale = new Vector3(
+                    direction > 0? Mathf.Abs(transform.localScale.x): -Mathf.Abs(transform.localScale.x),
+                    transform.localScale.y,
+                    transform.localScale.z);
+        }
+        else {
+            bool nowFaceRight = transform.localScale.x < 0;
+            if (nowFaceRight != direction > 0)
+                transform.localScale = new Vector3(
+                    direction > 0 ? -Mathf.Abs(transform.localScale.x) : Mathf.Abs(transform.localScale.x),
+                    transform.localScale.y,
+                    transform.localScale.z);
+        }
+    }
+
+    public void TryWalkTo(Vector3Int _destination) {
+        if (!boxCollider.DownTouched && !climbing)
+        {
+            waitOnGround = true;
+            destination = _destination;
+            return;
+        }
+        else
+            WalkTo(_destination);
+    }
+
+    public void WalkTo(Vector3Int _destination) {
+        Vector3Int position = NavigationManager.ins.EnvMap.WorldToCell(transform.position);
+
+        if (!climbing)
+            position.y -= 1;
+        routes = NavigationManager.ins.FindNavigateRoute(position, _destination);
+
+        for (int i = 0; i < routes.Length; i++) {
+            Debug.LogFormat("{0} {1}", routes[i].Type, routes[i].Position);
+        }
+
+        if (routes.Length > 1)
+        {
+            routePositions = new Vector3[routes.Length];
+
+            for (int i = 0; i < routes.Length; i++) {
+                if (routes[i].Type == RouteType.Ground)
+                    routes[i].Position.y += 1;
+
+                routePositions[i] = NavigationManager.ins.EnvMap.CellToWorld(routes[i].Position) + new Vector3(0.5f, 0);
+            }
+
+            destinationIndicator.transform.position = routePositions[routePositions.Length - 1];
+            destinationIndicator.SetActive(true);
+
+            moving = true;
+            routeI = 1;
+
+            Turn(routePositions[routeI].x.CompareTo(routePositions[routeI - 1].x));
+
+            if (routes[routeI - 1].Type == RouteType.Ground)
+                animator.PlayAnimation(AKWalk);
+            else
+                animator.PlayAnimation(AKClimb);
+        }
+        else if (moving) {
+            moving = false;
+            destinationIndicator.SetActive(false);
+            animator.PlayAnimation(AKIdle);
+        }
+    }
+
+    private void FixedUpdate() {
+        if (waitOnGround && boxCollider.DownTouched) {
+            waitOnGround = false;
+            WalkTo(destination);
+            return;
+        }
+
+        if (landWait.Running) {
+            if (landWait.FixedUpdateEnd) {
+                landWait.Running = false;
+                animator.PlayAnimation(moving? AKWalk: AKIdle);
+            }
+            return;
+        }
+
+        if (jumping) {
+            if (jumpStart) {
+                if (boxCollider.DownTouched) {
+                    jumping = false;
+
+                    animator.PlayAnimation(AKLand);
+                    landWait.Reset();
+                    return;
+                }
+            }
+            else {
+                if (!boxCollider.DownTouched)
+                    jumpStart = true;
+            }
+        }
+    
+        if (moving)
+            HandleMovement();
+    }
+
+    #region Movement Handling
+    private Vector2 ResolveVelocity(Vector2 velocity) {
+        if (velocity.x < 0 && boxCollider.LeftTouched) velocity.x = 0;
+        if (velocity.x > 0 && boxCollider.RightTouched) velocity.x = 0;
+
+        if (velocity.y < 0 && boxCollider.DownTouched) velocity.y = 0;
+        if (velocity.y > 0 && boxCollider.UpTouched) velocity.y = 0;
+
+        return velocity;
+    }
+
+    private void HandleMovement() {
+        if (routes[routeI].Type == RouteType.Ground)
+            HandleTwoGroundMoving();
+        else if (routes[routeI].Type == RouteType.Ladder)
+            HandleLadderClimbing();
+    }
+    
+    private void HandleNextMovenment() {
+        if (routeI + 1 >= routePositions.Length)
+        {
+            moving = false;
+            destinationIndicator.SetActive(false);
+
+            if (climbing)
+                animator.PlayAnimation(AKClimbIdle);
+            else
+                animator.PlayAnimation(AKIdle);
+
+            rigidbody.velocity = new Vector2(0, 0);
+        }
+        else {
+            switch (routes[routeI].Type)
+            {
+                case RouteType.Ground:
+                    if (routes[routeI + 1].Type == RouteType.Ladder){
+                        animator.PlayAnimation(AKClimb);
+                        rigidbody.velocity = Vector2.zero;
+                        rigidbody.gravityScale = 0;
+                        boxCollider.Collider.enabled = false;
+                        climbing = true;
+                    }
+                    break;
+                case RouteType.Ladder:
+                    if (routes[routeI + 1].Type == RouteType.Ground) {
+                        animator.PlayAnimation(AKWalk);
+                        rigidbody.velocity = Vector2.zero;
+                        rigidbody.gravityScale = gravityScale;
+                        boxCollider.Collider.enabled = true;
+                        climbing = false;
+                    }
+                    break;
+            }
+
+            routeI++;
+            Turn(routePositions[routeI].x.CompareTo(routePositions[routeI - 1].x));
+        }
+    }
+
+    private void HandleTwoGroundMoving() {
+        float routeDelta = routePositions[routeI].x - routePositions[routeI - 1].x;
+        float progress;
+
+        if (routeDelta == 0)
+            progress = 1;
+        else
+            progress = (transform.position.x - routePositions[routeI - 1].x) / routeDelta;
+
+        if (progress < 1) {
+            Vector2 velocity = rigidbody.velocity;
+            velocity.x = routePositions[routeI].x > routePositions[routeI - 1].x? walkSpeed: -walkSpeed;
+            if (jumping)
+                velocity.x *= airSpeedMultiplier;
+
+            if (routePositions[routeI].y > transform.position.y && !jumping && boxCollider.DownTouched) {
+                velocity.y = jumpForce;
+                jumping = true;
+                jumpStart = false;
+                animator.PlayAnimation(AKJump);
+            }
+
+            rigidbody.velocity = ResolveVelocity(velocity);
+        }
+        else
+            HandleNextMovenment();
+    }
+
+    private void HandleLadderClimbing() {
+        float routeDelta = routePositions[routeI].y - routePositions[routeI - 1].y;
+        float posDelta = transform.position.y - routePositions[routeI - 1].y;
+        float progress = posDelta / routeDelta;
+
+        if (progress < 1) {
+            Vector2 velocity = rigidbody.velocity;
+            velocity.y = routePositions[routeI].y > routePositions[routeI - 1].y ? climbSpeed : -climbSpeed;
+            rigidbody.velocity = velocity;
+        }
+        else
+            HandleNextMovenment();
+    }
+    #endregion
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        if (NavigationManager.ins != null && NavigationManager.ins.EnvMap != null && moving) {
+            Gizmos.color = Color.green;
+            for (int i = routeI; i < routePositions.Length; i++) {
+                Gizmos.DrawCube(routePositions[i], Vector3.one * 0.25f);
+                Gizmos.DrawLine(
+                    routePositions[i - 1],
+                    routePositions[i]);
+            }
+
+            Gizmos.DrawCube(routePositions[routePositions.Length - 1], Vector3.one * 0.5f);
+        }
+    }
+    #endif
+
+}
